48a9a322451d8084bf00a26f2d66f47e56272686
Ephy <87033515+EphyMusic@users.noreply.github.com>
2026-07-15 11:57:02 -0700

    still working on it, finished runrootservers, working on rununrootservers

--- basic.ts
+++ basic.ts
@@ -9,7 +9,9 @@ export async function main(ns: NS) {
 	ns.ui.openTail();
 	const startTime = Date.now() / 2000
 	while (true) {
-		await display(ns, startTime, serverCheckInit(ns));
+		const baseServers = serverCheckInit(ns)
+		const servers = [runRootServers(ns,baseServers[1]),[""],[""]]
+		await display(ns, startTime, servers);
 		await ns.sleep(20)
 	}
 }
@@ -57,34 +59,24 @@ function serverCheckInit(ns: NS): [ScannedServer[], ScannedServer[], ScannedServ
 	return [rooted, unrooted, owned];
 }
 
-async function display(ns: NS, startTime: number, servers: [ScannedServer[], ScannedServer[], ScannedServer[]]) {
-	const rooted = runRootServers(ns,servers[0]);
+async function display(ns: NS, startTime: number, servers: string[][]) {
+	const rooted = servers[0];
 	const unrooted = servers[1];
 	const owned = servers[2];
 	ns.clearLog()
 
-	ns.print("rooted servers:\n");
-	for (const s of rooted) {
+	const rootGroups:string[][] = makeGroup(rooted);
+	const unrootGroups:string[][] = makeGroup(unrooted);
 
+	const rGroupSel:number = Math.floor((Date.now() / 2000 - startTime) % rootGroups.length);
+	for (const s of rootGroups[rGroupSel]) {
+		ns.print(`${s}\n`)
 		ns.ui.renderTail();
 	}
 
-	const unrootGroups: ScannedServer[][] = [];
-	let group: ScannedServer[] = [];
-	ns.print("\nunrooted servers:\n");
-	let x = 0;
-	for (const s of unrooted) {
-		if (x >= 5) {
-			x = 0;
-			unrootGroups.push(group);
-			group = [];
-		}
-		group.push(s);
-		x += 1;
-	}
-	const groupSel = Math.floor((Date.now() / 2000 - startTime) % unrootGroups.length);
-	for (const s of unrootGroups[groupSel]) {
-		ns.print(`${s.server.hostname}\n`);
+	const unGroupSel:number = Math.floor((Date.now() / 2000 - startTime) % unrootGroups.length);
+	for (const s of unrootGroups[unGroupSel]) {
+		ns.print(`${s}\n`);
 		ns.ui.renderTail();
 	}
 	
@@ -102,12 +94,15 @@ function runRootServers(ns:NS,servers:ScannedServer[]):string[] {
 	const pWeak = "/payload/weaken.ts";
 	const pGrow = "/payload/grow.ts";
 	const pHack = "/payload/hack.ts";
-	const context:string[] = [];
+	const ctx1:string[] = []
+	const ctx2:string[] = []
+	const ctx3:string[] = []
+	const ctx4:string[] = []
 	for (const s of servers) {
 		const freeRam = s.server.maxRam - s.server.ramUsed;
-		let output:string;
 		const currentSec = s.server.hackDifficulty ?? 0;
-		const thresholdSec = (s.server.minDifficulty ?? 0) + 1;
+		const minimumSec = s.server.minDifficulty ?? 0;
+		const thresholdSec = minimumSec + 1;
 		const currentMoney = s.server.moneyAvailable ?? 0
 		const maxMoney = s.server.moneyMax ?? 0
 		if (ns.ps(s.server.hostname).length === 0) {
@@ -147,12 +142,19 @@ function runRootServers(ns:NS,servers:ScannedServer[]):string[] {
 				s.action = recallAction(p.filename)
 			}
 		}
-		if (rankAction(s.action) === 0) {
-			context.push(`${s.server.hostname} | $${s.server.moneyAvailable}/$${s.server.moneyMax}`)
+		const act = rankAction(s.action)
+		if (act === 0) {
+			ctx1.push(`${s.server.hostname} | $${ns.format.number(currentMoney,2)}/$${ns.format.number(maxMoney)} | ${ns.format.number(minimumSec, 2)}/${ns.format.number(currentSec,2)}/${ns.format.number(thresholdSec,2)} | ${s.action}`)
+		} else if (act === 1) {
+			ctx2.push(`${s.server.hostname} | $${ns.format.number(currentMoney,2)}/$${ns.format.number(maxMoney)} | ${ns.format.number(minimumSec, 2)}/${ns.format.number(currentSec,2)}/${ns.format.number(thresholdSec,2)} | ${s.action}`)
+		} else if (act === 2) {
+			ctx3.push(`${s.server.hostname} | $${ns.format.number(currentMoney,2)}/$${ns.format.number(maxMoney)} | ${ns.format.number(minimumSec, 2)}/${ns.format.number(currentSec,2)}/${ns.format.number(thresholdSec,2)} | ${s.action}`)
+		} else {
+			ctx4.push(`${s.server.hostname} | $${ns.format.number(currentMoney,2)}/$${ns.format.number(maxMoney)} | ${ns.format.number(minimumSec, 2)}/${ns.format.number(currentSec,2)}/${ns.format.number(thresholdSec,2)} | ${s.action}`)
 		}
-
+		
 	}
-
+	const context = [ctx1.join(`\n`),ctx2.join(`\n`),ctx3.join(`\n`),ctx4.join(`\n`)]
 	return context
 }
 
@@ -170,4 +172,21 @@ function rankAction(str:string):number {
 	if (str.includes("grow")) return 1;
 	if (str.includes("weak")) return 3;
 	return 4;
+}
+
+function makeGroup(servers:string[]):string[][] {
+	const fullGroup:string[][] = []
+	let group:string[] = []
+	let x = 0;
+	for (const s of servers) {
+		if (x >= 5) {
+			x = 0;
+			fullGroup.push(group);
+			group = [];
+		}
+		group.push(s);
+		x += 1;
+	}
+	fullGroup.push(group);
+	return fullGroup;
 }
