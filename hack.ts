727a651831c0a4ee600dd3ca97c5c3520cc31000
Ephy <87033515+EphyMusic@users.noreply.github.com>
2026-07-30 21:06:37 -0700

    hack.ts

---
+++ hack.ts
@@ -0,0 +1,22 @@
+import {NS} from "@ns";
+
+export async function main(ns:NS) {
+    if (ns.args.length > 0) {
+        const NA:string = "NULL PORT DATA";
+        const portNum = Number(ns.args[0]);
+        const port = ns.getPortHandle(portNum);
+        while (true) {
+            if (port.peek() !== NA) {
+                const info = port.read();
+                const threshSec = info[0];
+                const currSec = info[1];
+                const currMoney = info[3];
+                const threshMoney = info[5];
+                if ((currSec > threshSec) || (currMoney < threshMoney)) return;
+            }
+            await ns.hack();
+        }
+    } else {
+        while (true) await ns.hack()
+    }
+}
