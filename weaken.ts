4538172ab340728dd9ee619bd3be830ca060be4c
Ephy <87033515+EphyMusic@users.noreply.github.com>
2026-07-30 21:07:10 -0700

    weaken.ts

---
+++ weaken.ts
@@ -0,0 +1,20 @@
+// import {NS} from "@ns";
+
+export async function main(ns:NS) {
+    if (ns.args.length > 0) {
+        const NA:string = "NULL PORT DATA";
+        const portNum = Number(ns.args[0]);
+        const port = ns.getPortHandle(portNum);
+        while (true) {
+            if (port.peek() !== NA) {
+                const info = port.read();
+                const currSec = info[1];
+                const minSec = info[2];
+                if ((currSec === minSec)) return;
+            }
+            await ns.weaken();
+        }
+    } else {
+        while (true) await ns.weaken()
+    }
+}
