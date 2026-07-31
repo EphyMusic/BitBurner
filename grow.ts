9184f46c7f051f2b81a47c386ea1a3e3b1cef495
Ephy <87033515+EphyMusic@users.noreply.github.com>
2026-07-30 21:05:35 -0700

    grow.ts

---
+++ grow.ts
@@ -0,0 +1,22 @@
+// import {NS} from "@ns";
+
+export async function main(ns:NS) {
+    if (ns.args.length > 0) {
+        const NA:string = "NULL PORT DATA";
+        const portNum = Number(ns.args[0]);
+        const port = ns.getPortHandle(portNum);
+        while (true) {
+            if (port.peek() !== NA) {
+                const info = port.read();
+                const threshSec = info[0];
+                const currSec = info[1];
+                const currMoney = info[3];
+                const maxMoney = info[4];
+                if ((currSec > threshSec) || (currMoney === maxMoney)) return;
+            }
+            await ns.grow();
+        }
+    } else {
+        while (true) await ns.grow()
+    }
+}
